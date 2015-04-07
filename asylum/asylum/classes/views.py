44a0ffd72807e09be7f6ccb93ef3e0a39bb4c0d0
Steve Pomeroy <steve@staticfree.info>
2015-04-07 00:51:55 -0400

    Adds basic home view

--- asylum/asylum/classes/views.py
+++ asylum/asylum/classes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from asylum.classes.models import Session
 # Create your views here.
+
+def session_list(request):
+    sessions = Session.objects.all()
+    return render(request, 'session_list.html', { 'sessions': sessions })
--- asylum/asylum/urls.py
+++ asylum/asylum/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'asylum.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
      url(r'^schedule/', include('schedule.urls')),
+     url(r'^$', 'asylum.classes.views.session_list', name='home'),
 
     url(r'^admin/', include(admin_site.urls)),
 )
