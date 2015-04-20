66165bf67bdbd48bfb138bbc4d232fd33565f123
Steve Pomeroy <steve@staticfree.info>
2015-04-20 00:42:47 -0400

    Reworks day name display

--- asylum/asylum/classes/templatetags/schedule_extra.py
+++ asylum/asylum/classes/templatetags/schedule_extra.py
@@ -7,32 +7,73 @@ from django.utils.dateformat import format, time_format
 from django.utils.timezone import template_localtime
 register = template.Library()
 
+WEEKDAYS = set((0, 1, 2, 3, 4))
+WEEKENDDAYS = set((5, 6))
+
+ALL_DAYS = WEEKDAYS.union(WEEKENDDAYS)
+
 @register.filter
 def daynames(occurrences, arg=None):
     fmt = arg or 'l'
-    df = format
-    first = template_localtime(occurrences[0].start)
-    last = template_localtime(occurrences[-1].start)
+
+    dates = list(map(lambda occurrence: occurrence.start, occurrences))
+    days = list(map(lambda date: format(date, fmt), dates))
+    days_num = list(map(lambda date: date.weekday(), dates))
+
+    first_day = days[0]
+    last_day = days[-1]
 
     try:
-        if len(occurrences) == 1:
-            return df(first, fmt)
+        if len(days) == 0:
+            return ''
+
+        if len(days) == 1:
+            return first_day
+
+        # By doing it this way, we're guaranteed that the output represents the
+        # data regardless of the complexities of the repeating cycles.
+        unique_days = set(days_num)
+
+        if unique_days == WEEKDAYS:
+            return "Weekdays"
+
+        if unique_days == WEEKENDDAYS:
+            if len(days) == 2:
+                return "Weekend"
+            return "Weekends"
+
+        if unique_days == ALL_DAYS:
+            return "Daily"
+
+        if len(unique_days) == 1:
+            # e.g. "Wednesdays"
+            return "{0}s".format(first_day)
         else:
-            frequency = occurrences[0].event.rule.frequency
-            if frequency == 'WEEKLY':
-                # "Wednesdays"
-                return "{0}s".format(df(first, fmt))
-            elif frequency == 'DAILY':
-                if len(occurrences) == 2:
-                    return "{0} & {1}".format(df(first, fmt), df(last, fmt))
-                elif len(occurrences) > 7:
-                    return "Daily"
-                else:
-                    return "{0}–{1}".format(df(first, fmt), df(last, fmt))
-            elif frequency == 'MONTHLY':
-                return "Monthly"
+            if len(dates) == len(unique_days):
+                # e.g. "Wednesday & Friday"
+                # e.g. "Wednesday, Thursday & Friday"
+                return oxford_comma(days)
+            else:
+                # e.g. "Wednesdays & Fridays"
+                return oxford_comma(list(map(lambda d: "{0}s".format(d),
+                    days[:len(unique_days)])))
+
+        return ''
+
     except AttributeError:
         return ''
+
+def oxford_comma(items):
+    if len(items) == 0:
+        return ''
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return "{0} & {1}".format(*items)
+    else:
+        joinme = items[:-1]
+        joinme.append("& {0}".format(items[-1]))
+        return ", ".join(joinme)
 
 @register.filter
 def daterange(occurrences, arg=None):
